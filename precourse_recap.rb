a89f61615749a6feaa5cf5946044d1407a534d5a
michaeld55 <michaeld55@hotmail.co.uk>
2020-02-17 16:27:02 +0000

    Finish Homework

---
+++ precourse_recap.rb
@@ -0,0 +1,29 @@
+#* In the file, create an example of each of the following in Ruby:
+
+#1. Conditional (if, else)
+x = 0
+if x < 1
+  puts "X is less than One"
+else
+  puts "X is Greater than Zero"
+end
+
+#2. Function
+def my_function one, two
+  puts "This is function #{one} and is for #{two}"
+end
+first = "my_function"
+second = "Homework"
+my_function  first, second
+#3. Array
+my_array = ["text", 15, "sample"]
+
+#4. Hash
+
+favourite_things = { "food" => "cake", "animal" => "cat" }
+
+#5. Loop
+counter = 10
+while counter <= 0
+  counter = counter -1
+end
